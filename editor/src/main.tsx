1af8864312b30fc91dcc9e8ef639e522b3724ec6
yadasa <75089908+yadasa@users.noreply.github.com>
2026-08-28 09:46:02 -0500

    Wire Remotion editor application entrypoint

---
+++ editor/src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import {App} from './app';
+import './styles.css';
+
+const root = document.getElementById('root');
+if (!root) throw new Error('Clipper editor root element is missing');
+createRoot(root).render(<React.StrictMode><App /></React.StrictMode>);
